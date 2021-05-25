2c3c34e6886c02ec6828a4a91e870c5a3dda3959
qls19870612 <qls19870612@163.com>
2021-05-25 08:16:14 +0800

    设置标记

--- vue-sample/src/components/UpdateKLineChartBak.vue
+++ vue-sample/src/components/UpdateKLineChartBak.vue
@@ -10,15 +10,11 @@ import {dispose, init} from 'klinecharts'
 import generatedKLineDataList from '../generatedKLineDataList'
 import Layout from "@/Layout"
 
-
 export default {
   name: 'UpdateKLineChart',
   components: {Layout},
   mounted: function () {
-    let that = this;
-    window['setTitle'] = function (title) {
-      that.title = title;
-    };
+
     const kLineChart = init('update-k-line')
 
     // kLineChart.createTechnicalIndicator("MACD", false, {id: 'candle_pane'})
@@ -35,53 +31,36 @@ export default {
       // console.log("onReceiveDataInit:" + dataStr);
       let data = JSON.parse(dataStr);
 
-      for (const datum of data) {
-        if (datum.markText) {
-          createMark(datum);
-        }
-      }
       kLineChart.clearData();
       kLineChart.applyNewData(data);
 
     };
+    let thisData = this;
+    window['setTitle'] = function (title) {
+      thisData.title = title;
+    };
 
-    const annotationDrawExtend = function (ctx, coordinate, text, down) {
+    const annotationDrawExtend = function (ctx, coordinate, text) {
       ctx.font = '12px Roboto'
       ctx.fillStyle = '#2d6187'
       ctx.strokeStyle = '#2d6187'
-      const rectHeight = 28
-      let offsetStartY;
-      let offsetStartY1;
-      let rectOffsetY;
-      let rectTextOffsetY;
-      if (down) {
-        offsetStartY = 6 ;
-        offsetStartY1 = 5;
-        rectOffsetY = 0;
-        rectTextOffsetY = rectHeight / 2;
-      }
-      else
-      {
-        offsetStartY = -6 ;
-        offsetStartY1 = -5;
-        rectOffsetY = -rectHeight;
-        rectTextOffsetY = rectHeight / 2;
-      }
-      const textWidth = ctx.measureText(text).width
+      let measure = ctx.measureText(text)
+      const textWidth = measure.width
+      const textHeight = 26
+      const leftMove = 100
       const startX = coordinate.x
-      let startY = coordinate.y + offsetStartY
+      let startY = coordinate.y
       ctx.beginPath()
+
       ctx.moveTo(startX, startY)
-      startY += offsetStartY1
-      ctx.lineTo(startX - 4, startY)
-      ctx.lineTo(startX + 4, startY)
-      ctx.closePath()
+      ctx.lineTo(startX - 4-leftMove, startY)
       ctx.stroke()
 
-      const rectX = startX - textWidth / 2 - 6
-      const rectY = startY + rectOffsetY;
-      const rectWidth = textWidth + 12
 
+      const rectX = startX - textWidth / 2 - leftMove
+      const rectY = startY - textHeight / 2;
+      const rectWidth = textWidth;
+      const rectHeight = textHeight;
       const r = 2
       ctx.beginPath()
       ctx.moveTo(rectX + r, rectY)
@@ -90,33 +69,25 @@ export default {
       ctx.arcTo(rectX, rectY + rectHeight, rectX, rectY, r)
       ctx.arcTo(rectX, rectY, rectX + rectWidth, rectY, r)
       ctx.closePath()
-      ctx.stroke()
-      if (down) {
-        ctx.fillStyle = '#ece226'
-      }
-      else
-      {
-        ctx.fillStyle = '#9749c2'
-      }
+      ctx.fill()
 
+      ctx.fillStyle = '#fff'
       ctx.textBaseline = 'middle'
       ctx.textAlign = 'center'
-      ctx.fillText(text, startX, rectY + rectTextOffsetY)
+      ctx.fillText(text, rectX + textWidth/2, rectY + textHeight/2)
     }
 
     function createMark(candleItem) {
       let {timestamp, down, markText} = candleItem;
 
       let type = down ? "circle" : "rect";
-      let price = parseFloat(markText.substr(1));
-      console.log(`price:${price}`)
       kLineChart.createAnnotation([
         {
-          point: {timestamp: timestamp,price:price},
+          point: {timestamp: timestamp,price:candleItem.open},
           drawExtend: (params) => {
             const {ctx, coordinate} = params
 
-            annotationDrawExtend(ctx, coordinate, markText,down)
+            annotationDrawExtend(ctx, coordinate, markText)
           },
           styles: {
             symbol: {
@@ -125,7 +96,7 @@ export default {
             }
           }
         }, {
-          point: {timestamp: timestamp,price:price},
+          point: {timestamp: timestamp,price:candleItem.open},
           styles: {
             symbol: {
               type: type,
